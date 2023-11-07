fffb932e3379bc3962d9d0168cd21045fa5e72d3
Joshzie <Joshziebusiness@gmail.com>
2023-11-07 17:08:37 -0600

    Add files via upload

---
+++ beabadoobee.cpp
@@ -0,0 +1,94 @@
+/**
+* @name beabadoobee
+* @version Auto Updates
+* @description beabadoobee theme 
+* @author Joshzie
+* @youtube Joshzie
+* @twitter Joshzie
+* @donate https://www.paypal.me/Joshzie
+* @invite eNQSyZTMn4
+* @website https://twitter.com/Joshzie
+* @authorLink https://twitter.com/Joshzie
+* @authorId 764634315652726804 
+* @contact Joshzie#4344
+*/
+@import url("https://puckzxz.github.io/NotAnotherAnimeTheme/build/v3/naat.v3.css");
+@import url('https://fonts.googleapis.com/css2?family=Arial');
+@import url('https://codedotspectra.github.io/themes/core/custom-font.css');
+@import url('https://discordstyles.github.io/RadialStatus/dist/RadialStatus.css');
+:root {
+--main-theme-color: #FFFFFFFF;
+--theme-transparency: #121212CC;
+--theme-background-image: url('https://i.imgur.com/pHf66PH.jpg');
+--home-icon-image: url('https://i.imgur.com/TGncBiz.jpg');
+--home-icon-image-zoom: 100%;
+--home-icon-image-position: center;
+--server-listing-width: 72px;
+--font-size: 100%;
+--mention-color: #FAFAFA0D;
+--mention-sider: #FFFFFFFF;
+--mention-hover-color: #FFFFFF14;
+--category-hover-color: #FFFFFFFF;
+--unread-server-animation: none;
+--video-screen-button-background-color: #c8c8c833;
+--video-screen-button-foreground-color: #ffffffff;
+--video-screen-button-background-hovor-color: #FFFFFF66;
+--video-screen-button-foreground-hovor-color: #ffffffff;
+--member-listing-role-color: #FFFFFFFF;
+--small-user-popout-background-image: url('https://i.imgur.com/j1Z6HVc.jpg');
+--small-user-popout-background-transparency: #00000099;
+--small-user-popout-background-image-position: center;
+--big-user-popout-background-image: url('https://i.imgur.com/WucQt3t.png');
+--big-user-popout-background-transparency: #00000099;
+--big-user-popout-background-image-position: center;
+--code-markup-background-color: #00000099;
+--code-markup-border: 1px solid transparent;
+--message-box-transparency: #00000099;
+--embed-background-transparency: #00000099;
+--message-hover-buttons-background: #c8c8c81a;
+--context-menu-transparency: #0f0f0fcc;
+--add-discover-icon-transparency: #c8c8c826;
+--add-discover-icon-color: #FFFFFFFF;
+--channel-limit-count-color: #FFFFFFFF;
+--channel-limit-background-left: #c8c8c81a;
+--channel-limit-background-right: #c8c8c833;
+--scrollbar-color: #c8c8c833;
+--unread-message-divider-color: #F04747FF;
+--unread-message-divider-font-color: #F04747FF;
+--new-messages-bar-background-color: #000000FF;
+--new-messages-bar-font-color: #ffffff;
+--emoji-menu-background-color: #ffffff;
+--emoji-menu-sider-background-color: #ffffff;
+--emoji-menu-server-header-background-color: #ffffff;
+--emoji-menu-bottom-bar-background-color: #ffffff;
+--emoji-menu-search-bar-background-color: #ffffff;
+--message-search-menu-background-color: #000000cc;
+--message-search-container-background-color: #00000099;
+--message-search-result-background-color: #c8c8c80d;
+--text-voice-channel-color: #8e9297ff;
+--text-voice-channel-hover-color: #dcddde;
+--text-voice-channel-selected-text-color: #ffffff;
+--unread-text-channel-color: #ffffff;
+--unread-text-channel-bubble-color: #ffffff;
+--folder-background-color: #2f3136;
+--expanded-folder-background-color: #00000000;
+--reaction-emoji-background-color: #00000000;
+--reaction-emoji-border-color: 1px solid transparent;
+--reaction-emoji-hover-background-color: #ffffff1a;
+--reaction-emoji-hover-border-color: #ffffff33;
+--rs-small-spacing: 2px;
+--rs-medium-spacing: 3px;
+--rs-large-spacing: 4px;
+--rs-small-width: 2px;
+--rs-medium-width: 3px;
+--rs-large-width: 4px;
+--rs-avatar-shape: 50%;
+--rs-online-color: #43b581;
+--rs-idle-color: #faa61a;
+--rs-dnd-color: #F00000FF;
+--rs-offline-color: #FFFFFFFF;
+--rs-streaming-color: #643da7;
+--rs-invisible-color: #FFFFFFFF;
+--rs-phone-visible: block;
+--customFont: 'Arial';
+}
